f676b897b8a5610bbd49452326f2add140d4bc90
slqpez <santiago.lopez16@udea.edu.co>
2021-07-06 11:12:47 -0500

    Setup.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import ReactPlayer from "react-player"
+
+function App() {
+
+  
+  return <div className="App">
+   <ReactPlayer
+        url="https://www.youtube.com/watch?v=e4YPCnYQ95E&list=RDSoFxZwOwWQo&"
+      />
+  </div>;
+}
+
+export default App;
